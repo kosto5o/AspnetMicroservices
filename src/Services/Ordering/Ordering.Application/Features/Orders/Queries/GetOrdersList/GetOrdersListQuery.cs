3f229b93fa99001d5d10dff13a0548bc9e2f125e
Kosto <kosto5o@gmail.com>
2021-08-06 14:10:09 +0200

    Ordering.App Commands & Queries

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            this.UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+
+        
+    }
+}
